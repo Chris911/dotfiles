ec9803eb9a49b8fe622ddc1cb038e36e595225cd
Christophe Naud-Dulude <christophe.nauddulude@shopify.com>
2021-11-30 17:06:40 -0500

    oh-my-zsh + antigen + spin utils

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+mkdir ~/.zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -L git.io/antigen > ~/.zsh/antigen.zsh
+
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh ~/.zsh
 
 source ~/.zshrc
+
+printf '\n%s\n\t%s\n' '[alias]' 'co = checkout' >> ~/.gitconfig
 
 exit 0
