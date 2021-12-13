ffc2ae708a59c7aae7e0771ae2059df1c362f913
Christophe Naud-Dulude <christophe.nauddulude@shopify.com>
2021-12-13 17:39:43 -0500

    Add spin aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -34,3 +34,15 @@ alias gpr='open "https://github.com/$(git config --get remote.origin.url | cut -
 alias gsp='git stash pop'
 alias gauf='git add $(git ls-files -o --exclude-standard)'
 alias gsc='git diff --name-only --diff-filter=U'
+
+# Spin
+if [ "$SPIN" ]; then
+    alias procs='systemctl list-units "proc*"'
+    alias core-stop='systemctl stop proc-shopify--shopify@server.service'
+    alias sfr-stop='systemctl stop proc-shopify--storefront-renderer@server.service'
+    alias core-start='systemctl start proc-shopify--shopify@server.service'
+    alias sfr-start='systemctl start proc-shopify--storefront-renderer@server.service'
+    alias debug-sfr='PRY=1 bundle exec rake server_isospin'
+    alias sfr-logs='journalctl -fu proc-shopify--storefront-renderer@server.service'
+    alias core-logs='journalctl -fu proc-shopify--shopify@server.service'
+fi
