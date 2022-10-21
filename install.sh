964ede1be081e21fd351410d14de3ba04aea42ac
Christophe Naud-Dulude <christophe.nauddulude@shopify.com>
2022-10-21 10:22:31 -0400

    Add seeding script

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ source ~/.zshrc
 
 printf '\n%s\n\t%s\n\t%s\n' '[alias]' 'co = checkout' 'branch-name = "!git rev-parse --abbrev-ref HEAD"' >> ~/.gitconfig
 
+SHOPIFY_DIR="${HOME}/src/github.com/Shopify/shopify"
+SCRIPT="${HOME}/.data/cartridges/default/seed.sh"
+
+cd ${SHOPIFY_DIR}
+shadowenv exec --dir ${SHOPIFY_DIR} -- ${SCRIPT}
+
 exit 0
